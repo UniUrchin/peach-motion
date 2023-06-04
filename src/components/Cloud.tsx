0ed6a3776d5f11f96a2eea5a590659ac1b15b44e
UniUrchin <85057870+UniUrchin@users.noreply.github.com>
2023-06-04 13:51:52 +0900

    :dizzy: 雲の速度をずらした

--- src/components/Cloud.tsx
+++ src/components/Cloud.tsx
@@ -6,7 +6,7 @@ export const Cloud1: React.FC = () => {
       src="/cloud.png"
       className="w-[8rem] h-[5rem] top-[10rem] absolute z-30"
       animate={{ x: [-300, 300] }}
-      transition={{ duration: 15, ease: "linear", repeat: Infinity }}
+      transition={{ duration: 18, ease: "linear", repeat: Infinity }}
     />
   );
 };
@@ -17,7 +17,7 @@ export const Cloud2: React.FC = () => {
       src="/cloud.png"
       className="w-[6rem] h-[4rem] top-[20rem] absolute z-30"
       animate={{ x: [300, -300] }}
-      transition={{ duration: 15, delay: 5, ease: "linear", repeat: Infinity }}
+      transition={{ duration: 15, delay: 3, ease: "linear", repeat: Infinity }}
     />
   );
 };
